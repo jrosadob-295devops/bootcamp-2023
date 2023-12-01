eab36d32b4bc9e933e545c32c8005b042ddaf207
Javier A. Rosado Baltazar <jrosadob@hotmail.com>
2023-12-01 17:25:05 -0500

    Fix ...

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "deploy ..."
 source libraries.sh
 
-apache2_install()
-apache2_setup()
+apache2_install
 
-Repository="devops-static-web"
-GitUrl="https://github.com/roxsross/$Repository.git"
-clone_repository $Repository $GitUrl
+echo ""
 
-install_web $Repository
+# apache2_setup()
+# Repository="devops-static-web"
+# GitUrl="https://github.com/roxsross/$Repository.git"
+# clone_repository $Repository $GitUrl
+
+# install_web $Repository
 
--- libraries.sh
+++ libraries.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-package_exist() {
-  if dpkg -l | grep -q $1 ;
-    then
-        return 1
-    else
-        return 0
-    fi
-}
+# package_exist() {
+#   if dpkg -l | grep -q $1;
+#     then
+#         return 1
+#     else
+#         return 0
+#     fi
+# }
 
-package_install() {
-    apt install -y $1 $2 $3 $4 $5 $6 $7 $8 $9
-}
+# package_install() {
+#     apt install -y $1 $2 $3 $4 $5 $6 $7 $8 $9
+# }
 
 apache2_install() {
+    echo "apache2_install ..."
     $PACKAGE="apache2"
     package_exist $PACKAGE
-    if [ $? -eq 1 ]
+    if [ $? -eq 1 ];
     then
         echo "$PACKAGE ya está instalado"
     else
@@ -25,21 +26,24 @@ apache2_install() {
     fi
 }
 
-apache2_setup() {
-    systemctl enable apache2
-    systemctl start apache2
-}
+# apache2_setup() {
+#     systemctl enable apache2
+#     systemctl start apache2
+# }
 
-clone_repository() {
-    if [ -d "$1" ];
-    then
-        echo "Repositorio ya existe."
-        git pull
-    else
-        git clone -b devops-mariobros $2
-    fi
-}
+# clone_repository() {
+#     if [ -d "$1" ];
+#     then
+#         echo "Repositorio ya existe."
+#         git pull
+#     else
+#         git clone -b devops-mariobros $2
+#     fi
+# }
 
-install_web() {
-    cp -r $1/* /var/www/html
-}
+# install_web() {
+#     cp -r $1/* /var/www/html
+# }
+
+
+
